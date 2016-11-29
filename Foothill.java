74f8e32c56e699bea19751c83789fe513823de9d
eric kuehnl <eric.kuehnl@gmail.com>
2016-11-28 23:49:57 -0800

    The original commit for Assignment_01

---
+++ Foothill.java
@@ -0,0 +1,15 @@
+
+public class Foothill
+{
+   public static void main(String[] args)
+   {
+      System.out.println("Hello World!");
+   }
+}
+
+/*---------------paste of run from console window ------------------
+
+Hello World!
+
+------------------------------------------------------------------*/
+
